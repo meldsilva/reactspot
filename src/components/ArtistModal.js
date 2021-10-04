f0dc3f576488dcf3a82b3b78dadb7310dbf97f4d
mdsilva <mdsilva@costargroup.com>
2021-10-04 16:18:25 -0400

    Fixed issue with multiple modals displaying where a track had multiple artists

--- src/components/ArtistModal.js
+++ src/components/ArtistModal.js
@@ -5,18 +5,19 @@ import axios from "axios";
 import { capitalize } from "../utils/helpers"
 import img404 from '../assets/images/img_not_found.gif';
 
-const ArtistModal = ({ show, closeModal, artistid }) => {
+const ArtistModal = ({ show, closeModal, artistid, activeArtist }) => {
 
   const [loading, setLoading] = useState(false);
   const [artist, setArtist] = useState({});
   const token = localStorage.getItem('token');
   const uri = `https://api.spotify.com/v1/artists/${artistid}`;
 
-  console.log("Show: ",show);
-  console.log("Artist ID: ", artistid);
-  console.log("Artist Resp Data: ", artist);
-  console.log("Artist Image URL: ", artist.images);
-  console.log("Artist Popularity: ", artist.popularity);
+  // console.log("Show: ",show);
+  // console.log("Artist ID: ", artistid);
+  // console.log("activeArtist: ", activeArtist);
+  // console.log("Artist API Response: ", artist);
+  // console.log("Artist Image URL: ", artist.images);
+  // console.log("Artist Popularity: ", artist.popularity);
 
   const handleHide = () => {
     closeModal();
@@ -34,7 +35,7 @@ const ArtistModal = ({ show, closeModal, artistid }) => {
           });
          
           setArtist(response.data);
-          console.log("Artists Response", response.data);
+          // console.log("Artists Response", response.data);
       }
       catch(error) {
           console.log(error);
@@ -44,7 +45,7 @@ const ArtistModal = ({ show, closeModal, artistid }) => {
       }
   }
   fetchData()
-  },[artistid, uri, token]);
+  },[]);
 
     
   if (Object.keys(artist).length === 0) {
@@ -57,47 +58,45 @@ const ArtistModal = ({ show, closeModal, artistid }) => {
     ReactDOM.createPortal(      
         <React.Fragment>
           <BootModal centered 
-            show={show} 
+            show={show && (artistid === activeArtist)} 
             onHide={handleHide} 
             size="sm"
-            // dialogClassName="modal-90w"
-            // aria-labelledby="example-custom-modal-styling-title"
-            >
-            <BootModal.Header closeButton>
-              <BootModal.Title>{artist.name}</BootModal.Title>
-            </BootModal.Header>
-            <BootModal.Body>
-              <Card className="card" >
-                <Card.Img
-                  variant="top" 
-                  src= {artist.images.length > 0 ? artist.images[0].url : img404} />
-                <Card.Body>
-                  <Card.Text style={{ fontSize: 13, fontFamily: "Arial" }}>
-                    <span>Followers: {artist.followers.total.toLocaleString()} </span><br/>
-                    <span>
-                      <ProgressBar now={artist.popularity} label={`Popularity ${artist.popularity}`} />
-                    </span>
+          >
+          <BootModal.Header closeButton>
+            <BootModal.Title>{artist.name}</BootModal.Title>
+          </BootModal.Header>
+          <BootModal.Body>
+            <Card className="card" >
+              <Card.Img
+                variant="top" 
+                src= {artist.images.length > 0 ? artist.images[0].url : img404} />
+              <Card.Body>
+                <Card.Text style={{ fontSize: 13, fontFamily: "Arial" }}>
+                  <span>Followers: {artist.followers.total.toLocaleString()} </span><br/>
+                  <span>
+                    <ProgressBar now={artist.popularity} label={`Popularity ${artist.popularity}`} />
+                  </span>
 
-                    {
-                      artist.genres.map( (genre, idx)  =>
-                        <span style={{padding: 1}}>
-                        <Badge pill 
-                        key={idx}
-                        bg={idx % 2 === 0  ? 'primary' : 'secondary'}>
-                          {`${capitalize(genre)}`}</Badge>
-                        </span>
-                      )
-                    }
-                    {/* <br/> */}
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </BootModal.Body>
-            {/* <BootModal.Footer>
-              <Button variant="secondary" onClick={handleHide}>
-                Close
-              </Button>
-            </BootModal.Footer> */}
+                  {
+                    artist.genres.map( (genre, idx)  =>
+                      <span style={{padding: 1}}>
+                      <Badge pill 
+                      key={idx}
+                      bg={idx % 2 === 0  ? 'primary' : 'secondary'}>
+                        {`${capitalize(genre)}`}</Badge>
+                      </span>
+                    )
+                  }
+                  {/* <br/> */}
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </BootModal.Body>
+          {/* <BootModal.Footer>
+            <Button variant="secondary" onClick={handleHide}>
+              Close
+            </Button>
+          </BootModal.Footer> */}
           </BootModal>
         </React.Fragment>,
         document.body
--- src/components/ArtistName.js
+++ src/components/ArtistName.js
@@ -6,9 +6,12 @@ import { Button } from 'react-bootstrap';
 const ArtistName = (artists) => {
     // Loop through the array and create a badge-like component instead of a comma-separated string
     const [show, showModal] = useState(false);
-    const doModal = () => {
+    const [activeArtist, setActiveArtist] = useState('');
+    const doModal = (event) => {
         console.log("Called Tracks.doModal");
+        console.log("Event info: ",event.target);
         showModal(true);
+        setActiveArtist(event.currentTarget.id);
     }
     const hideModal = () => {
         console.log("Called Tracks.hideModal");
@@ -18,9 +21,10 @@ const ArtistName = (artists) => {
     return (       
         
         artists.artists.map( (artist, idx) => (
-        <>
+        <React.Fragment>
             <Button variant="link" 
                 key={idx.toString()}
+                id={artist.id}
                 onClick={doModal}>
                     {artist.name}
 
@@ -29,9 +33,10 @@ const ArtistName = (artists) => {
                 show={show}
                 closeModal={hideModal}
                 artistid={artist.id} 
+                activeArtist={activeArtist}
                 key={Math.random.toString()}
             />
-        </>
+        </React.Fragment>
         ))
     )
 }
