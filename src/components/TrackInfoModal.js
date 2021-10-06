54f8e7316a5d0b19f4185309e8d6d0e0eccfacc7
meldsilva <meldsilva@gmail.com>
2021-10-06 00:08:00 -0400

    Track info modal WIP

--- src/components/TrackInfoModal.js
+++ src/components/TrackInfoModal.js
@@ -7,80 +7,57 @@ import img404 from '../assets/images/img_not_found.gif';
 
 const TrackInfoModal = ({ show, closeModal, track }) => {
 
-  const [loading, setLoading] = useState(false);
-  const [artist, setArtist] = useState({});
-  const token = localStorage.getItem('token');
-  // const uri = `https://api.spotify.com/v1/artists/${artistid}`;
-
   console.log("TrackInfo Modal show value: ", show);
+  console.log("Trackid: ", track.id);
+  // console.log("URL: ", track.external_urls.spotify);
 
   const handleHide = () => {
     closeModal();
   }
 
-  // useEffect( () => {
-  //     setLoading(true);
-
-  //     async function fetchData(){
-  //     try {
-  //         const response = await axios.get(uri, {
-  //             headers: {
-  //                 'Authorization': `Bearer ${token}`
-  //             }
-  //         });
-         
-  //         setArtist(response.data);
-  //         // console.log("Artists Response", response.data);
-  //     }
-  //     catch(error) {
-  //         console.log(error);
-  //     }
-  //     finally {
-  //         setLoading(false);
-  //     }
-  // }
-  // fetchData()
-  // },[]);
-
-  // if (Object.keys(artist).length === 0) {
-  //       return <p>No Data!</p>
-  // }  
-  // if (loading) {
-  //   return <p>Loading. Please wait...</p>;
-  // }  
+  if (Object.keys(track).length === 0) {
+    return null;// return <p>No Data!</p>
+  }    
   return(
     ReactDOM.createPortal(      
         <React.Fragment>
           <BootModal centered 
             show={show} 
             onHide={handleHide} 
-            size="sm"
-          >
-          <BootModal.Header closeButton>
-            <BootModal.Title>Artist Name</BootModal.Title>
-          </BootModal.Header>
+            size="sm">
+          {/*<BootModal.Header closeButton>*/}
+          {/*  <BootModal.Title>{track.name}</BootModal.Title>*/}
+          {/*</BootModal.Header>*/}
           <BootModal.Body>
             <Card className="card" >
-              {/* <Card.Img
-                variant="top" 
-                src= {artist.images.length > 0 ? artist.images[0].url : img404} /> */}
+                <Card.Img
+                    variant="top"
+                    src= {track.album.images.length > 0 ? track.album.images[0].url : img404} />
               <Card.Body>
                 <Card.Text style={{ fontSize: 13, fontFamily: "Arial" }}>
-                  {/* <span>Followers: {artist.followers.total.toLocaleString()} </span><br/>
-                  <span>
-                    <ProgressBar now={artist.popularity} label={`Popularity ${artist.popularity}`} />
-                  </span> */}
-                  {/* {
-                    artist.genres.map( (genre, idx)  =>
-                      <span style={{padding: 1}}>
-                      <Badge pill 
-                      key={idx}
-                      bg={idx % 2 === 0  ? 'primary' : 'secondary'}>
-                        {`${capitalize(genre)}`}</Badge>
-                      </span>
-                    )
-                  } */}
-                  {/* <br/> */}
+                    {/*<iframe src={track.preview_url} width="100%" height="380"*/}
+                    {/*        frameBorder="0" allowFullScreen=""*/}
+                    {/*        allow="clipboard-write; encrypted-media; fullscreen; picture-in-picture">*/}
+                    {/*</iframe>*/}
+                    <iframe src={track.preview_url} width="100%" height="77"
+                            frameBorder="0" allowFullScreen=""
+                            allow="clipboard-write; encrypted-media; fullscreen; picture-in-picture">
+                    </iframe>
+                    <br/>
+                    {
+
+                        track.album.artists.map( (track) => (
+
+                            <span>{track.name +=  ' | '.slice(3)}</span>
+                        )
+
+
+
+                            // (<><span>{track.name}</span> <br/></>)
+                            // {track.name + '|'}
+                        )
+                    }<br />
+                    {/*{track.id}*/}
                 </Card.Text>
               </Card.Body>
             </Card>
