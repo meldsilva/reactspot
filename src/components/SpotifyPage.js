829b005909fbbaf030bc80be8e28176755a22b8d
mdsilva <mdsilva@costargroup.com>
2021-10-14 15:00:56 -0400

    Now active link is highlighted

--- src/components/SpotifyPage.js
+++ src/components/SpotifyPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Outlet} from "react-router-dom";
+import { Outlet} from "react-router-dom";
 import { Container } from "react-bootstrap";
 import { StyledLink, StyledNav } from "./SpotifyPageStyles";
 
@@ -9,10 +9,10 @@ const SpotifyPage = () => {
         <div style={{backgroundColor: "black", height: "30%"}}>
         <h2>Spotify Page</h2>
         <StyledNav>
-            <StyledLink to="playlists">Playlists</StyledLink>
-            <StyledLink to="podcasts">Podcasts</StyledLink>
-            {/* <StyledLink to="artists">Artists</StyledLink> */}
-            <StyledLink to="albums">Albums</StyledLink> 
+            <StyledLink activeClassName="active" to="playlists">Playlists</StyledLink>
+            <StyledLink activeClassName="active" to="podcasts">Podcasts</StyledLink>
+            <StyledLink activeClassName="active" to="albums">Albums</StyledLink> 
+            <StyledLink activeClassName="active" to="newreleases">New Releases</StyledLink> 
         </StyledNav>
         </div>
         <Container fluid="xl">
--- src/components/SpotifyPageStyles.js
+++ src/components/SpotifyPageStyles.js
@@ -1,52 +1,21 @@
-import {Link} from "react-router-dom";
+import '../components/SpotifyPages.css';
+import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
-export const StyledLink = styled(Link)`
+export const StyledLink = styled(NavLink)`
     color: #1DB954;
     text-decoration: none;
     margin: 1rem;
     position: relative;
     font-weight: bold;
 `;
+
 export const StyledNav = styled.nav`
     float:top;
     text-decoration:none;
-    color: 5c0059; 
+    // color: 5c0059; 
     font-size:20px; 
     padding: 5px 0px; 
     display:inline-block; 
     transition: all 0.5s ease 0s;
-`;
-
-export const StyledTable = styled.table`
-    font-family: arial, sans-serif;
-    border-collapse: collapse;
-    table-layout: fixed;
-    width: 80%;
-`;
-
-export const StyledTableHeader = styled.thead`
-  border: 1px solid #dddddd;
-  text-align: left;
-  padding: 8px;
-  color: black;
-  background: #719a9b;
-  font-size: 18px;
-  font-weight: bold;
-  text-align: center;
-  width: 100%;
-`;
-
-export const StyledTableCell = styled.td`
-  border: 1px solid #dddddd;
-
-  text-align:
-      ${prop => prop.align? prop.align: "left"};
-  padding: 8px;
-  font-size: 15px;
-  table-layout: fixed;
-`;  
-
-export const StyledTableRow = styled.tr`
-    background-color: ${prop => prop.row % 2 ? '#dae8e6' : '#ffffff'};
 `;
