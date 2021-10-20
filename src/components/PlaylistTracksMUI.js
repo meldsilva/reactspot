060e8e7617796ec2ecb38f5ffce2d963838f05cd
meldsilva <meldsilva@gmail.com>
2021-10-19 21:07:09 -0400

    Audio control ready as small rounded play icon

--- src/components/PlaylistTracksMUI.js
+++ src/components/PlaylistTracksMUI.js
@@ -6,6 +6,7 @@ import MaterialTable from "material-table";
 import ArtistName from './ArtistName';
 import { ThemeProvider } from '@material-ui/core';
 import AudioPlayer from 'material-ui-audio-player';
+import { makeStyles } from '@material-ui/core';
 
 
 const header_style = {
@@ -16,6 +17,25 @@ const header_style = {
     width: 800,
     maxWidth: 800
   }
+  const useStyles = makeStyles(theme => ({
+    playIcon: {
+        color: '#1DB954',
+        '&:hover': {
+          color: 'tomato'
+        }
+    }
+}));  
+
+//   const useStyles = () => makeStyles(
+//       {
+//         playIcon: {
+//             color: '#f50057',
+//             '&:hover': {
+//               color: '#ff4081'
+//             }
+//       }
+//     });
+      
 
 function PlaylistTracksMUI() {
 
@@ -38,16 +58,15 @@ function PlaylistTracksMUI() {
                 
                 title: 'Artist',
                 render: rowData => {
-                    console.log(rowData.track.artists[0].name);
-                    {return <ArtistName 
+                        return <ArtistName 
                         artists={rowData.track.artists}
-                        key={Math.random().toString()} />}
+                        key={Math.random().toString()} />
                     }
             },
             {
                 title: 'Date Added',
                 field: 'added_at',
-                // type: 'date'
+                type: 'datetime'
             },
             {
                 title: 'Preview',
@@ -55,13 +74,22 @@ function PlaylistTracksMUI() {
                 
                 render: rowData => {
                     {
-                        // {console.log("track.preview_url", rowData.track.preview_url)}
                         return(
-                            //  <p>{rowData.track.preview_url}</p>
-                            <AudioPlayer
-                            rounded={true}
-                            src={rowData.track.preview_url}
-                          />
+     
+                                <AudioPlayer
+                                  elevation={1}
+                                  rounded={true}
+                                  width="56px"
+                                  variation="primary"
+                                  download={false}
+                                  volume={false}
+                                  displaySlider={false}
+                                  useStyles={useStyles}
+                                //   loop={loop}
+                                //   spacing={spacing}
+                                  debug={false}
+                                  src={rowData.track.preview_url}
+                                />
                             // <audio controls style={{width: 110, height: 25, color: 'green'}}
                             //    useRef="audio_tag" autoPlay={false}
                             //    src={rowData.track.preview_url} Play/>                          
@@ -71,54 +99,6 @@ function PlaylistTracksMUI() {
             }
         ]
     };
-
-    // const columns = React.useMemo(
-    //     () => [
-    //             // {
-    //             //     Header: 'Track',
-    //             //     align: 'left',
-    //             //     accessor: row => `${row.track.name}:${row.track.id}`,
-    //             //     Cell: ({ cell: { value } }) =>
-    //             //         <Fragment>
-    //             //             <Trackinfo key={Math.random.toString()} trackinfo={value} />
-    //             //         </Fragment>
-    //             // },
-    //             {
-    //                 Header: 'Track',
-    //                 accessor: 'track.name',
-    //                 maxWidth: 150
-
-    //             },
-    //             {
-    //                 Header: 'Album',
-    //                 accessor: 'track.album.name',
-    //                 maxWidth: 150,
-    //             },
-    //             {
-    //                 Header: 'Artist',
-    //                 accessor: 'track.artists',
-    //                 Cell: ({ cell: { value } }) =>
-    //                     <ArtistName key={Math.random.toString()} artists={value} />
-    //             },
-    //             {
-    //                 Header: 'Date Added',
-    //                 accessor: 'added_at',
-    //                 maxWidth: 150,
-    //                 Cell: ({ cell: { value } }) => {  const dt = new Date(value); return dt.toLocaleString() }
-    //             },
-    //         {
-    //             Header: 'Preview',
-    //             accessor: 'track.preview_url',
-    //             Cell: ({ cell: { value } }) =>
-    //                 <div>
-    //                     <audio controls style={{width: 110, height: 25, color: 'green'}}
-    //                            useRef="audio_tag" autoPlay={false}
-    //                            src={value} Play/>
-    //                 </div>
-    //         }
-    //      ],
-    //     []
-    // );
 
     const [loading, setLoading] = useState(false);
     const [tracks, setTracks] = useState([]);
@@ -137,7 +117,7 @@ function PlaylistTracksMUI() {
            
             setTracks(response.data.items);
             // console.log("Tracks Response", response.data);
-            console.log("Response Data Items",response.data.items);
+            // console.log("Response Data Items",response.data.items);
         }
         catch(error) {
             console.log(error);
@@ -152,9 +132,6 @@ function PlaylistTracksMUI() {
     if (loading) {
         return <LoadingPage />;
     }
-    // if (Object.keys(tracks).length === 0) {
-    //     return <p>No Data!</p>
-    // }
 
     if (tracks.length === 0){
         return <p>No Data!</p>
@@ -164,7 +141,17 @@ function PlaylistTracksMUI() {
         // <Container fluid="xl">
         <div>
             <h4 className="text-muted">{playlistname}</h4>
-            <MaterialTable columns={table_header.columns} data={tracks} />
+            <MaterialTable 
+            columns={table_header.columns} 
+            data={tracks}
+            options={{
+                sorting: true,
+                search: true,
+                pageSize: 20,
+                showTitle: false,
+                maxBodyHeight: '500px',
+                searchFieldAlignment: 'left'}}
+            />
         </div>
     )
 }
