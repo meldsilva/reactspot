4229d1a63249a09853f03077fca728e6a3c4ba65
meldsilva <meldsilva@gmail.com>
2021-10-23 11:40:11 -0400

    Sticky nav bar work in progress

--- src/components/SpotifyPage.js
+++ src/components/SpotifyPage.js
@@ -6,15 +6,25 @@ import { StyledLink, StyledNav } from "./SpotifyPageStyles";
 const SpotifyPage = () => {
     return(
         <React.Fragment>
-            <div style={{backgroundColor: "black", height: "6%", width: '100%', position: 'fixed'}}>
+            <div style={{backgroundColor: "black", height: "7%", width: '100%', position: 'fixed'}}>
                 <StyledNav>
                     <StyledLink activeclassname="active" to="playlists">Playlists</StyledLink>
                     <StyledLink activeclassname="active" to="podcasts">Podcasts</StyledLink>
                     <StyledLink activeclassname="active" to="albums">Albums</StyledLink> 
                     <StyledLink activeclassname="active" to="newreleases">New Releases</StyledLink> 
                 </StyledNav>
-                <Outlet />
             </div>
+            {/* <div style={{
+                    // position: 'absolute', 
+                    position: 'absolute',
+                    width: '90%',
+                    top: '7%', 
+                    padding: '5px'
+                    }}
+            > */}
+                <Outlet />
+            {/* </div> */}
+
         </React.Fragment>
     );
 }
